b19b1cead86c7c5b34a68f31dd723cc625f23fad
MartinSvoren71 <79747942+MartinSvoren71@users.noreply.github.com>
2023-03-20 18:47:14 +0100

    Update ask_ai.py

--- ask_ai.py
+++ ask_ai.py
@@ -2,9 +2,43 @@ import os
 from threading import Thread
 from llama_index import SimpleDirectoryReader, GPTListIndex, readers, GPTSimpleVectorIndex, LLMPredictor, PromptHelper
 from langchain import OpenAI
-from main import api_kx
 
-api_k = api_kx
+
+api_k = *
+
+def initialize_ai(api_key):
+    os.environ["*"] = api_key
+    os.environ[api_k] = api_key
+
+
+def construct_index(directory_path):
+@ -15,7 +15,7 @@ def construct_index(directory_path):
+    max_chunk_overlap = 20
+    chunk_size_limit = 600
+
+    llm_predictor = LLMPredictor(llm=OpenAI(temperature=0.5, model_name="text-curie-001", max_tokens=num_outputs, openai_api_key="*"))
+    llm_predictor = LLMPredictor(llm=OpenAI(temperature=0.7, model_name="davinci", max_tokens=num_outputs, openai_api_key=api_k))
+
+    documents = SimpleDirectoryReader(directory_path).load_data()
+
+@ -29,7 +29,46 @@ def construct_index(directory_path):
+
+
+def ask_ai(question):
+    os.environ["OPENAI_API_KEY"] = "*"
+    os.environ["OPENAI_API_KEY"] = api_k
+    index = GPTSimpleVectorIndex.load_from_disk('index.json')
+    response = index.query(question, response_mode="compact")
+    return response.response
+
+
+
+import os
+from threading import Thread
+from llama_index import SimpleDirectoryReader, GPTListIndex, readers, GPTSimpleVectorIndex, LLMPredictor, PromptHelper
+from langchain import OpenAI
+
+api_k = *
 
 def initialize_ai(api_key):
     os.environ[api_k] = api_key
@@ -17,19 +51,13 @@ def construct_index(directory_path, theme):
     chunk_size_limit = 600
 
     llm_predictor = LLMPredictor(llm=OpenAI(temperature=0.7, model_name="davinci", max_tokens=num_outputs, openai_api_key=api_k))
-
+    
     documents = SimpleDirectoryReader(directory_path).load_data()
 
-    index = GPTSimpleVectorIndex(documents, llm_predictor=llm_predictor, prompt_helper=prompt_helper)
-
-    index.save_to_disk('index.json')
-    
     if theme == "chameleon":
         index_file = "indexCH.json"
     elif theme == "compex":
         index_file = "indexCompex.json"
-    elif theme == "powerline":
-        index_file = "index.json"
     else:
         index_file = "index.json"
 
@@ -43,5 +71,3 @@ def ask_ai(question, theme):
     index = construct_index("directory_path", theme)
     response = index.query(question, response_mode="compact")
     return response.response
-
-
