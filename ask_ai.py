c52908e5cfaa5abb41d619c18286ab06b7ecc082
MartinSvoren71 <79747942+MartinSvoren71@users.noreply.github.com>
2023-03-21 19:03:55 +0100

    Update ask_ai.py

--- ask_ai.py
+++ ask_ai.py
@@ -24,7 +24,7 @@ def construct_index(directory_path):
 def ask_ai(question):
     os.environ["OPENAI_API_KEY"] = api_k
     index = GPTSimpleVectorIndex.load_from_disk('indexCH.json')
-    response = index.query(question, response_mode="top_3")
+    response = index.query(question, response_mode="verbose")
     return response.response
 
 
