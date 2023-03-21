4dc6f4e524ac75b7e6429e5209975206b55603b4
MartinSvoren71 <79747942+MartinSvoren71@users.noreply.github.com>
2023-03-21 21:37:36 +0100

    cs

--- app2.py
+++ app2.py
@@ -7,24 +7,20 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 @app.route('/ask', methods=['POST'])
-def ask_question():
+def ask():
     question = request.form['question']
     theme = request.form['theme']
+    key = request.form['key']  # Get the value of the key from the form
+
     if question.strip().lower() == 'exit':
         return redirect(url_for('home'))
-    else:
+    elif key == "xxx007":  # Check if the key is "xxx007"
         response = ask_ai(question, theme)  # Pass the theme value
         return render_template('response.html', question=question, theme=theme, response=response)
-    t = Thread(target=initialize_ai)
-    t.start()
-    app.run(host='0.0.0.0', port='5000')    
-  #  pass  # added to avoid indentation error
-if __name__ == '__main__':
-    openai_api_key = api_kx or (len(sys.argv) > 1 and sys.argv[1])
-    if not openai_api_key:
-        print("Error: No OpenAI API key provided. Set the OPENAI_API_KEY environment variable or pass the key as a command-line argument.")
-        sys.exit(1)
-    t = Thread(target=initialize_ai, args=(openai_api_key,))
-    t.start()
-    app.run(host='0.0.0.0', port='5000')
+    else:
+        return "Invalid key. Please try again."
+
+t = Thread(target=initialize_ai)
+t.start()
+app.run(host='0.0.0.0', port='5000')
     
