b282a5ce4fe990d2cc013873ff9da79d8ec556a5
MartinSvoren71 <79747942+MartinSvoren71@users.noreply.github.com>
2023-03-21 20:38:53 +0100

    s

--- app2.py
+++ app2.py
@@ -1,16 +1,11 @@
-
 from flask import Flask, request, render_template, redirect, url_for
 from ask_ai import initialize_ai, ask_ai
 from threading import Thread
-api_k = "*"
 from main import api_kx
-
 app = Flask(__name__)
-
 @app.route('/')
 def home():
     return render_template('index.html')
-
 @app.route('/ask', methods=['POST'])
 def ask_question():
     question = request.form['question']
@@ -20,20 +15,15 @@ def ask_question():
     else:
         response = ask_ai(question, theme)  # Pass the theme value
         return render_template('response.html', question=question, theme=theme, response=response)
-
-
     t = Thread(target=initialize_ai)
     t.start()
-    app.run(host='0.0.0.0', port='5000')
-      
+    app.run(host='0.0.0.0', port='5000')    
   #  pass  # added to avoid indentation error
-
 if __name__ == '__main__':
     openai_api_key = api_kx or (len(sys.argv) > 1 and sys.argv[1])
     if not openai_api_key:
         print("Error: No OpenAI API key provided. Set the OPENAI_API_KEY environment variable or pass the key as a command-line argument.")
         sys.exit(1)
-
     t = Thread(target=initialize_ai, args=(openai_api_key,))
     t.start()
     app.run(host='0.0.0.0', port='5000')
--- ask_ai.py
+++ ask_ai.py
@@ -3,38 +3,25 @@ from threading import Thread
 from llama_index import SimpleDirectoryReader, GPTListIndex, readers, GPTSimpleVectorIndex, LLMPredictor, PromptHelper
 from langchain import OpenAI
 from main import api_kx
-
-
 api_k = api_kx
-
 def initialize_ai(api_key):
     os.environ[api_k] = api_key
-
-
 def construct_index(directory_path):
     max_chunk_overlap = 20
     chunk_size_limit = 600
-
-
     llm_predictor = LLMPredictor(llm=OpenAI(temperature=0.2, model_name="gpt-3.5-turbo", max_tokens=num_outputs, openai_api_key=api_k))
-
     documents = SimpleDirectoryReader(directory_path).load_data()
-
-
 def ask_ai(question, theme):
     os.environ["OPENAI_API_KEY"] = api_k
-
     index_file = {
         "chameleon": "indexChameleon.json",
         "compex": "indexCompex.json",
         "chameleondiscovery": "indexChameleonDiscovery.json",
         "innova": "indexInnova.json",
         "powerline": "indexPowerLine.json"
-
     }.get(theme, "indexCH.json")  # Default to "indexCH.json" if the theme value is not recognized
-
     index = GPTSimpleVectorIndex.load_from_disk(index_file)
-    response = index.query(question, response_mode="compact")
+    response = index.query(question, theme, response_mode="compact")
     return response.response
 
 
