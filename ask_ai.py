2461ea756283df13b91de4ceefff22a92b7ac383
MartinSvoren71 <79747942+MartinSvoren71@users.noreply.github.com>
2023-03-22 23:38:26 +0100

    d

--- ask_ai.py
+++ ask_ai.py
@@ -1,12 +1,11 @@
 import os
-from datetime import datetime  # import datetime module
 from threading import Thread
+from datetime import datetime
 from llama_index import SimpleDirectoryReader, GPTListIndex, readers, GPTSimpleVectorIndex, LLMPredictor, PromptHelper
 from langchain import OpenAI
 from main import api_kx
-
 api_k = api_kx
-log_file = "log.txt"  # specify the log file path and name
+log_file = "/home/ec2-user/bot/log.txt"
 
 def initialize_ai(api_key):
     os.environ[api_k] = api_key
@@ -29,16 +28,15 @@ def ask_ai(question, theme):
          "univet": "indexUnivet.json",
         "kymera": "indexKymera.json",
         "zygomx": "indexZygoMX.json"
-
     }.get(theme, "indexCH.json")  # Default to "indexCH.json" if the theme value is not recognized
     index = GPTSimpleVectorIndex.load_from_disk(index_file)
     response = index.query(question, response_mode="compact")
     
     # log the question, answer, and time to the log file
     with open(log_file, "a") as f:
+        f.write(f"Timestamp: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n")
         f.write(f"Question: {question}\n")
         f.write(f"Answer: {response.response}\n")
-        f.write(f"Timestamp: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n")
         f.write("=================================\n")
     
     return response.response
