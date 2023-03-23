805339570ed4737331f52905413dc4f19b6824d3
MartinSvoren71 <79747942+MartinSvoren71@users.noreply.github.com>
2023-03-23 18:57:55 +0100

    Update app2.py

--- app2.py
+++ app2.py
@@ -4,6 +4,7 @@ from threading import Thread
 from main import api_kx
 
 app = Flask(__name__)
+
 @app.route('/')
 def home():
     return render_template('indexSplit.html')
@@ -15,17 +16,16 @@ def ask():
     key = request.form['key']  # Get the value of the key from the form
 
     if question.strip().lower() == 'exit':
-    return redirect(url_for('home'))
-elif key == "xxx007":  # Check if the key is "xxx007"
-    response = ask_ai(question, theme)  # Pass the theme value     
-    with open('index.txt', 'r') as file:
-        content = file.read()
-    return render_template('indexSplit.html', question=question, theme=theme, response=response, key=key)
-else:
-    return render_template('badkey.html')
+        return redirect(url_for('home'))
+    elif key == "xxx007":  # Check if the key is "xxx007"
+        response = ask_ai(question, theme)  # Pass the theme value     
+        with open('index.txt', 'r') as file:
+            content = file.read()
+        return render_template('indexSplit.html', question=question, theme=theme, response=response, key=key)
+    else:
+        return render_template('badkey.html')
 
 
 t = Thread(target=initialize_ai)
 t.start()
 app.run(host='0.0.0.0', port='5000')
-    
