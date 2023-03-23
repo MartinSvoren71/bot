53c75df825667373fbe8ef11f1e09df285682f21
MartinSvoren71 <79747942+MartinSvoren71@users.noreply.github.com>
2023-03-23 18:02:44 +0100

    ds

--- clonelogfile.py
+++ clonelogfile.py
@@ -1,14 +1,14 @@
 import shutil
 
-        def clone_file(source_path, destination_path, force_overwrite=False):
-    try:
-        if force_overwrite:
-            shutil.copy2(source_path, destination_path)
-        else:
-            shutil.copy2(source_path, destination_path + '/' + os.path.basename(source_path))
+    def clone_file(source_path, destination_path, force_overwrite=False):
+try:
+    if force_overwrite:
+        shutil.copy2(source_path, destination_path)
+    else:
+        shutil.copy2(source_path, destination_path + '/' + os.path.basename(source_path))
         print("File cloned successfully!")
-    except Exception as e:
-        print(f"Error: {e}")
+except Exception as e:
+    print(f"Error: {e}")
         
 
         
