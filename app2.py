26fe1e683db8928c2119d0beecd208da6d1b7526
MartinSvoren71 <79747942+MartinSvoren71@users.noreply.github.com>
2023-03-23 20:07:38 +0100

    Update app2.py

--- app2.py
+++ app2.py
@@ -1,4 +1,5 @@
-from flask import Flask, request, render_template, redirect, url_for
+from flask import Flask, request, render_template, redirect, url_for, jsonify
+
 from ask_ai import initialize_ai, ask_ai
 from threading import Thread
 from main import api_kx
