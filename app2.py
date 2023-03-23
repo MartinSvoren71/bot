070ec193ff8788b8225f275d4fdad44c49b76d2a
MartinSvoren71 <79747942+MartinSvoren71@users.noreply.github.com>
2023-03-23 22:59:09 +0100

    Update app2.py

--- app2.py
+++ app2.py
@@ -3,7 +3,6 @@ from ask_ai import initialize_ai, ask_ai
 from threading import Thread
 from main import api_kx
 import os
-
 app = Flask(__name__)
 
 @app.route('/display', methods=['GET'])
@@ -11,7 +10,8 @@ def display():
     question = request.args.get('question')
     theme = request.args.get('theme')
     response = request.args.get('response')
-    return render_template('indexSplit.html', question=question, theme=theme, response=response)
+    key = request.args.get('key')
+    return render_template('indexSplit.html', question=question, theme=theme, response=response, key=key)
 
 @app.route('/log-content')
 def log_content():
@@ -21,23 +21,41 @@ def log_content():
     return content
 
 @app.route('/')
+
+@def home():
+    if question.strip().lower() == 'exit':
+        return redirect(url_for('home'))
+    elif key == "xxx007":  # Check if the key is "xxx007"
+        return render_template('indexSplit.html')
+       # return render_template('indexSplit.html', question=question, theme=theme, response=response, key=key)
+    else:
+        return render_template('badkey.html')
+    
 def home():
     file_path = os.path.join(os.getcwd(), 'log.txt')
     with open(file_path, 'r') as file:
         content = file.read()
-    return render_template('login.html')
+    return render_template('indexSplit.html', content=content)
 
 @app.route('/ask', methods=['POST'])
 def ask():
     question = request.form['question']
     theme = request.form['theme']
     key = request.form['key']  # Get the value of the key from the form
-    if key == "xxx007":  # Check if the key is "xxx007"
+    #response = ask_ai(question, theme)  # Pass the theme value
+
+    if question.strip().lower() == 'exit':
+        return redirect(url_for('home'))
+    elif key == "xxx007":  # Check if the key is "xxx007"
         response = ask_ai(question, theme)  # Pass the theme value      
-        return redirect(url_for('display', question=question, theme=theme, response=response))
+       # return render_template('indexSplit.html', question=question, theme=theme, response=response, key=key)
     else:
         return render_template('badkey.html')
+
 
 t = Thread(target=initialize_ai)
 t.start()
 app.run(host='0.0.0.0', port='5000')
+
+
+
