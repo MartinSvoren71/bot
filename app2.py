34fcb79523842a0ccb977fc0e3fdb43adacdece6
MartinSvoren71 <79747942+MartinSvoren71@users.noreply.github.com>
2023-03-24 16:27:04 +0100

    Update app2.py

--- app2.py
+++ app2.py
@@ -53,15 +53,14 @@ def log_content():
 
 @app.route('/ask', methods=['POST'])
 def ask():
-    key = "nnp"
     question = request.form['question']
     theme = request.form['theme']
-    key = request.form['key']  # Get the value of the key from the form
+    key = "nnp"
     if key == "nnp":  # Check if the key is "xxx007"
         response = ask_ai(question, theme)  # Pass the theme value
         return render_template('indexSplit.html', question=question, theme=theme, response=response, key=key)
     else:
-        return render_template('indexSplit.html', question=question, theme=theme)
+        return render_template('bad_key.html', question=question, theme=theme)
 t = Thread(target=initialize_ai)
 t.start()
 app.run(host='0.0.0.0', port=5000)
