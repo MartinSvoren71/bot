6a6ec08da5fdc8abc612247f1e7735f619652ca0
MartinSvoren71 <79747942+MartinSvoren71@users.noreply.github.com>
2023-03-24 19:24:09 +0100

    ds

--- ask_ai.py
+++ ask_ai.py
@@ -6,6 +6,7 @@ from langchain import OpenAI
 from main import api_kx
 import datetime
 api_k = api_kx
+import json
 
 def initialize_ai(api_key):
     os.environ[api_k] = api_key
@@ -16,19 +17,16 @@ def construct_index(directory_path):
     llm_predictor = LLMPredictor(llm=OpenAI(temperature=0.9, model_name="gpt-3.5-turbo", max_tokens=num_outputs, openai_api_key=api_k))
     documents = SimpleDirectoryReader(directory_path).load_data()
 
+    
 def ask_ai(question, theme):
+    # Load the theme file names from the themes.json file
+    with open('themes.json', 'r') as f:
+        themes = json.load(f)
+
+    # Get the file name for the current theme
+    index_file = themes.get(theme, "indexCH.json")
+    
     os.environ["OPENAI_API_KEY"] = api_k
-    index_file = {
-        "chameleon": "indexChameleon.json",
-        "compex": "indexCompex.json",
-        "chameleondiscovery": "indexChameleonDiscovery.json",
-        "innova": "indexInnova.json",
-        "powerline": "indexPowerLine.json",
-        "newton": "indexNewton.json",
-        "univet": "indexUnivet.json",
-        "kymera": "indexKymera.json",
-        "zygomx": "indexZygoMX.json"
-    }.get(theme, "indexCH.json")
     index = GPTSimpleVectorIndex.load_from_disk(index_file)
     response = index.query(question, response_mode="compact")
     log_file = os.path.join(os.getcwd(), 'log.txt')
