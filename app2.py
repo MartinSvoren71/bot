ce3b86b4d45ad9763b98666876ef53172051fd5d
MartinSvoren71 <79747942+MartinSvoren71@users.noreply.github.com>
2023-03-24 19:39:55 +0100

    Update app2.py

--- app2.py
+++ app2.py
@@ -32,18 +32,23 @@ def bad_key():
 @app.route("/indexSplit", methods=["GET", "POST"])
 def index():
     if "logged_in" in session:
-        # Load the themes from the themes.json file
-        with open('themes.json', 'r') as f:
-            themes = json.load(f)
+        return render_template("indexSplit.html")
+    else:
+        flash("Please log in first")
+        return redirect(url_for("login"))
 
-        # Generate the <option> elements dynamically
-        options = ''.join([f'<option value="{theme}">{theme_name}</option>' for theme, theme_name in themes.items()])
+    # Load the themes from the themes.json file
+    with open('themes.json', 'r') as f:
+        themes = json.load(f)
 
-        # Render the HTML with the dynamic <option> elements
-        html = f'''
-        <select name="theme" id="theme" onchange="saveTheme()">
-            {options}
-        </select>
+    # Generate the <option> elements dynamically
+    options = ''.join([f'<option value="{theme}">{theme_name}</option>' for theme, theme_name in themes.items()])
+
+    # Render the HTML with the dynamic <option> elements
+    html = f'''
+    <select name="theme" id="theme" onchange="saveTheme()">
+        {options}
+    </select>
         '''
         return render_template("indexSplit.html")
     else:
