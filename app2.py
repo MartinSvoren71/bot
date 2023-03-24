4de533bc14fca4f3beb9ce64bc79d9c8de2489e1
MartinSvoren71 <79747942+MartinSvoren71@users.noreply.github.com>
2023-03-24 15:36:49 +0100

    Update app2.py

--- app2.py
+++ app2.py
@@ -1,46 +1,9 @@
-
 from flask import Flask, request, render_template, redirect, url_for, jsonify
 from ask_ai import initialize_ai, ask_ai
 from threading import Thread
 from main import api_kx
 import os
-
 app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return redirect(url_for('login'))
-
-@app.route("/", methods=["GET", "POST"])
-def login():
-    if request.method == "POST":
-        password = request.form["password"]
-
-        if password == "correct_password":
-            session["logged_in"] = True
-            session.permanent = True
-            app.permanent_session_lifetime = timedelta(hours=1)
-            return redirect(url_for("index"))
-        else:
-            flash("Bad key provided")
-            return redirect(url_for("bad_key"))
-
-    return render_template("login.html")
-
-@app.route("/bad_key")
-def bad_key():
-    return render_template("badkey.html")
-
-@app.route("/index")
-def index():
-    if "logged_in" in session:
-        return render_template("indexSplit.html")
-    else:
-        flash("Please log in first")
-        return redirect(url_for("login"))
-
-if __name__ == "__main__":
-    app.run(debug=True)
 
 @app.route('/display', methods=['GET'])
 def display():
@@ -50,19 +13,40 @@ def display():
     key = request.args.get('key')
     return render_template('indexSplit.html', question=question, theme=theme, response=response, key=key)
 
+@app.route('/log-content')
+def log_content():
+    file_path = os.path.join(os.getcwd(), 'log.txt')
+    with open(file_path, 'r') as file:
+        content = file.read()
+    return content
+
+@app.route('/')
+def home():
+    file_path = os.path.join(os.getcwd(), 'log.txt')
+    with open(file_path, 'r') as file:
+        content = file.read()
+    return render_template('indexSplit.html', content=content)
+
 @app.route('/ask', methods=['POST'])
 def ask():
     question = request.form['question']
     theme = request.form['theme']
-    key = request.form['key']
+    key = request.form['key']  # Get the value of the key from the form
+    #response = ask_ai(question, theme)  # Pass the theme value
+
     if question.strip().lower() == 'exit':
-        return redirect(url_for('indexSplit'))
-    elif key == 'xxx007':
-        response = ask_ai(question, theme)
-        return render_template('indexSplit.html', question=question, theme=theme, response=response, key=key)
+        return redirect(url_for('home'))
+    elif key == "xxx007":  # Check if the key is "xxx007"
+        response = ask_ai(question, theme)  # Pass the theme value      
+       # return render_template('indexSplit.html', question=question, theme=theme, response=response, key=key)
     else:
-        return redirect(url_for('wrong_key'))
+        return render_template('badkey.html')
+
 
 t = Thread(target=initialize_ai)
 t.start()
 app.run(host='0.0.0.0', port='5000')
+
+
+
+
