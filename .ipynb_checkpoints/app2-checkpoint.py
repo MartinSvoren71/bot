ef1417a95ebdafda0b6e0cebc3bf7cbef23971ac
MartinSvoren71 <79747942+MartinSvoren71@users.noreply.github.com>
2023-03-25 23:43:37 +0100

    fd

--- .ipynb_checkpoints/app2-checkpoint.py
+++ .ipynb_checkpoints/app2-checkpoint.py
@@ -81,9 +81,11 @@ def ask():
         else :
             response = ask_ai(question, theme)  # Pass the theme value
             pdf_url = "https://s3.eu-north-1.amazonaws.com/knowledgevortex/s3/data/ChameleonDiscovery/Chameleon_Discovery_TPC_1313627_RevAC_press_covers.pdf"
+           
+            response = requests.post("http://localhost:5000/indexSplit.html", data={"pdf_url": pdf_url})
+            return jsonify({"pdf_url": pdf_url})
 
             return render_template('indexSplit.html', question=question, theme=theme, response=response, key=key)
-            return jsonify({"pdf_url": pdf_url})
     else:
         return render_template('bad_key.html', question=question, theme=theme)
 t = Thread(target=initialize_ai)
