d26126e6717cffebb14a98ad8631d492b1544f0a
MartinSvoren71 <79747942+MartinSvoren71@users.noreply.github.com>
2023-03-25 16:07:40 +0100

    fd

--- .ipynb_checkpoints/ask_ai-checkpoint.py
+++ .ipynb_checkpoints/ask_ai-checkpoint.py
@@ -19,46 +19,36 @@ def construct_index(directory_path):
 
     
 def ask_ai(question, theme):
-    # Load the theme file names from the themes.json file
-    with open('themes.json', 'r') as f:
-        themes = json.load(f)
-
-    # Get the file name for the current theme
-    index_file = themes.get(theme, "indexCH.json")
     os.environ["OPENAI_API_KEY"] = api_k
+    prompt = f"{theme}: {question}"
+    response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=prompt,
+        max_tokens=150,
+        n=1,
+        stop=None,
+        temperature=0.9,
+    )
 
-    if theme == "general":
-        prompt = f"{theme}: {question}"
-        response = openai.Completion.create(
-            engine="gpt-3.5-turbo",
-            prompt=prompt,
-            max_tokens=150,
-            n=1,
-            stop=None,
-            temperature=0.6,
-        )
-        answer = response.choices[0].text.strip()
-        response.response = answer 
-    else:
-        index = GPTSimpleVectorIndex.load_from_disk(index_file)
-        response = index.query(question, response_mode="compact")
-        
+    answer = response.choices[0].text.strip()
 
     log_file = os.path.join(os.getcwd(), 'log.txt')
-
+    
     # Read the existing data in the log file
     with open(log_file, "r") as f:
         existing_data = f.read()
-
+    
     # Write the new data followed by the existing data
     with open(log_file, "w") as f:
         f.write(f"Time: {datetime.datetime.now()}\n")
         f.write(f"Theme: {theme}\n")
         f.write(f"Question: {question}\n")
-        f.write(f"Answer: {answer}\n")
+        f.write(f"Answer: {response.response}\n")
         f.write("======================================================================================\n")
-        f.write("                         Knowledge Vortex v1.1                                 \n")
+        f.write("                         Knowlege Vortex v1.1                                 \n")
         f.write("======================================================================================\n")
         f.write(existing_data)
-
+        
     return response.response
+
+response.response
