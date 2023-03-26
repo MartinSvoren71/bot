d9a3184fab90a2d3380b591a0e1d7e3841205b79
MartinSvoren71 <79747942+MartinSvoren71@users.noreply.github.com>
2023-03-26 01:18:09 +0100

    fd

--- ask_GPT.py
+++ ask_GPT.py
@@ -9,23 +9,26 @@ import datetime
 api_k = api_kx
 import json
 
-def initialize_GPT(api_key):
-    os.environ[api_k] = api_k
-    openai.api_key = api_k
+def initialize_ai(api_key):
+    os.environ["OPENAI_API_KEY"] = api_kx
+    openai.api_key = api_kx
 
+initialize_ai(api_k)
 
-    
-def ask_GPT(question, theme):
-    # Load the theme file names from the themes.json file
-    with open('themes.json', 'r') as f:
-        themes = json.load(f)
+def ask_ai(question, theme):
+    os.environ["OPENAI_API_KEY"] = api_kx
+    prompt = f"{question}"
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=prompt,
+        max_tokens=1550,
+        n=1,
+        stop=None,
+        temperature=0.6,
+    )
 
-    # Get the file name for the current theme
-    index_file = themes.get(theme, "indexCH.json")
-    
-    os.environ["OPENAI_API_KEY"] = api_k
-    index = GPTSimpleVectorIndex.load_from_disk(index_file)
-    response = index.query(question, response_mode="compact")
+    answer = response.choices[0].text.strip()
+
     log_file = os.path.join(os.getcwd(), 'log.txt')
     
     # Read the existing data in the log file
@@ -37,11 +40,10 @@ def ask_GPT(question, theme):
         f.write(f"Time: {datetime.datetime.now()}\n")
         f.write(f"Theme: {theme}\n")
         f.write(f"Question: {question}\n")
-        f.write(f"Answer: {response.response}\n")
+        f.write(f"Answer: {answer}\n")  # Replace response.answer with answer
         f.write("======================================================================================\n")
         f.write("                         Knowlege Vortex v1.1                                 \n")
         f.write("======================================================================================\n")
         f.write(existing_data)
         
-    return response.response
-
+    return answer
