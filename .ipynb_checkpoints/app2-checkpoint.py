d009db14a44dee4f7deba9376e76aa607bccfd1c
MartinSvoren71 <79747942+MartinSvoren71@users.noreply.github.com>
2023-03-26 12:26:06 +0200

    fd

--- .ipynb_checkpoints/app2-checkpoint.py
+++ .ipynb_checkpoints/app2-checkpoint.py
@@ -6,11 +6,36 @@ from main import api_kx
 from datetime import timedelta
 import os
 import json
+import boto3
+from botocore.exceptions import ClientError
+aws_access_key_id = 'AKIA5BVJA3S5MNPVO2MP'
+aws_secret_access_key = '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'
+aws_default_region = 'eu-central-1'
+
+s3_client = boto3.client(
+    's3',
+    aws_access_key_id=aws_access_key_id,
+    aws_secret_access_key=aws_secret_access_key,
+    region_name=aws_default_region
+)
 
 app = Flask(__name__)
 app.secret_key = "xxx007"
 pdf_urla="https://knowledgevortex.s3.eu-north-1.amazonaws.com/s3/data/ChameleonDiscovery/Chameleon_Discovery_TPC_1313627_RevAC_press_covers.pdf"
 pdf_url=pdf_urla
+
+@app.route('/list_files')
+def list_files():
+    bucket_name = 'knowledgewortex'
+    folder_path = '/s3/data/'
+    
+    try:
+        response = s3_client.list_objects_v2(Bucket=bucket_name, Prefix=folder_path)
+    except ClientError as e:
+        return {'error': str(e)}
+    
+    files = [{'Key': item['Key'], 'Size': item['Size']} for item in response.get('Contents', [])]
+    return {'files': files}
 
 
 @app.route("/", methods=["GET", "POST"])
