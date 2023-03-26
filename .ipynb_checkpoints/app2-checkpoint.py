bd7e26772083d38803a7324b71a6d6fc35c9fe7b
MartinSvoren71 <79747942+MartinSvoren71@users.noreply.github.com>
2023-03-26 21:46:12 +0200

    fsd

--- .ipynb_checkpoints/app2-checkpoint.py
+++ .ipynb_checkpoints/app2-checkpoint.py
@@ -66,7 +66,7 @@ def index():
         contents = s3_client.list_objects(Bucket=BUCKET_NAME)
         files = contents['Contents']
         for file in files:
-        file['PresignedURL'] = generate_presigned_url(BUCKET_NAME, file['Key'])
+            file['PresignedURL'] = generate_presigned_url(BUCKET_NAME, file['Key'])
         return render_template("indexSplit.html", html=html, files=files)
     else:
         flash("Please log in first")
@@ -106,10 +106,10 @@ def ask():
     if key == "nnp":  # Check if the key is "xxx007"
         if theme == "general" :
             response = ask_GPT(question)  # Pass the theme value
-            return render_template('indexSplit.html', question=question, response=response, key=key, files=files)
+            #return render_template('indexSplit.html', question=question, response=response, key=key, files=files)
         else :
             response = ask_ai(question, theme)  # Pass the theme value
-            return render_template('indexSplit.html', question=question, theme=theme, response=response, key=key, files=files)
+            #return render_template('indexSplit.html', question=question, theme=theme, response=response, key=key, files=files)
     else:
         return render_template('bad_key.html', question=question, theme=theme)
 t = Thread(target=initialize_ai)
