2cc8b0334d5711c0ca2b9a5b9ca3bfc5111de047
MartinSvoren71 <79747942+MartinSvoren71@users.noreply.github.com>
2023-03-26 01:27:49 +0100

    d

--- .ipynb_checkpoints/ask_GPT-checkpoint.py
+++ .ipynb_checkpoints/ask_GPT-checkpoint.py
@@ -13,7 +13,7 @@ def initialize_GPT(api_key):
     os.environ["OPENAI_API_KEY"] = api_kx
     openai.api_key = api_kx
 
-initialize_ai(api_k)
+initialize_GPT(api_k)
 
 def ask_GPT():
     os.environ["OPENAI_API_KEY"] = api_kx
