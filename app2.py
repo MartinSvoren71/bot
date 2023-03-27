e80902d34f46ebe5e80e8729fa8b3fdb85144d69
MartinSvoren71 <79747942+MartinSvoren71@users.noreply.github.com>
2023-03-27 12:37:16 +0200

    d

--- app2.py
+++ app2.py
@@ -67,7 +67,7 @@ def index():
         files = contents['Contents']
         for file in files:
             file['PresignedURL'] = generate_presigned_url(BUCKET_NAME, file['Key'])
-        return render_template("index.html", html=html, files=files)
+        return render_template("indexSplit.html", html=html, files=files)
     else:
         flash("Please log in first")
         return redirect(url_for("login"))
@@ -112,7 +112,6 @@ def ask():
             return render_template('indexSplit.html', question=question, theme=theme, response=response, key=key, files=files)
     else:
         return render_template('bad_key.html', question=question, theme=theme)
-    
 t = Thread(target=initialize_ai)
 t.start()
 app.run(host='0.0.0.0', port=5000)
