3eba74fc07a26ac3421309cd943ae0617f83d69c
MartinSvoren71 <79747942+MartinSvoren71@users.noreply.github.com>
2023-03-29 22:21:54 +0200

    ds

--- .ipynb_checkpoints/app2-checkpoint.py
+++ .ipynb_checkpoints/app2-checkpoint.py
@@ -119,31 +119,30 @@ def ask():
     else:
         return render_template('bad_key.html', question=question, theme=theme)
     
-def search_pdf_files(keyword, file_paths):
+def search_pdf_files(keyword, directory):
     results = {}
     encrypted_files = []  # List to store encrypted files
-
-    for filepath in file_paths:
-        try:
-            file_obj = s3_client.get_object(Bucket=BUCKET_NAME, Key=filepath)
-            pdf_file = io.BytesIO(file_obj['Body'].read())
-
-            pdf_reader = PdfFileReader(pdf_file)
-            if pdf_reader.isEncrypted:
-                print(f"Skipping encrypted file: {filepath}")
-                encrypted_files.append(filepath)  # Add the encrypted file to the list
-                continue
-            for page_num in range(pdf_reader.getNumPages()):
-                text = pdf_reader.getPage(page_num).extractText()
-                pattern = re.compile(r'(?<=\.)([^.]*\b{}\b[^.]*(?:\.[^.]*){{0,1}})'.format(keyword))
-                matches = pattern.findall(text)
-                if matches:
-                    if filepath not in results:
-                        results[filepath] = []
-                    results[filepath].extend([(page_num, match) for match in matches])
-
-        except Exception as e:
-            print(f"Error processing {filepath}: {str(e)}")
+    for root, _, files in os.walk(directory):
+        for file in files:
+            if file.endswith('.pdf'):
+                filepath = os.path.join(root[len(directory) - 1:], file)
+                try:
+                    with open(filepath, 'rb') as pdf_file:
+                        pdf_reader = PdfFileReader(pdf_file)
+                        if pdf_reader.isEncrypted:
+                            print(f"Skipping encrypted file: {filepath}")
+                            encrypted_files.append(filepath)  # Add the encrypted file to the list
+                            continue
+                        for page_num in range(pdf_reader.getNumPages()):
+                            text = pdf_reader.getPage(page_num).extractText()
+                            pattern = re.compile(r'(?<=\.)([^.]*\b{}\b[^.]*(?:\.[^.]*){{0,1}})'.format(keyword))
+                            matches = pattern.findall(text)
+                            if matches:
+                                if filepath not in results:
+                                    results[filepath] = []
+                                results[filepath].extend([(page_num, match) for match in matches])
+                except Exception as e:
+                    print(f"Error processing {filepath}: {str(e)}")
     return results, encrypted_files
 
 
