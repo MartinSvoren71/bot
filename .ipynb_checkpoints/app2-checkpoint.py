498194c2c7e984fdae9363f4879a330f8bac5c13
MartinSvoren71 <79747942+MartinSvoren71@users.noreply.github.com>
2023-03-29 18:52:22 +0200

    ds

--- .ipynb_checkpoints/app2-checkpoint.py
+++ .ipynb_checkpoints/app2-checkpoint.py
@@ -5,12 +5,15 @@ from threading import Thread
 from main import api_kx
 from datetime import timedelta
 import os
+import re
 import json
 import subprocess
 import boto3
+from PyPDF4 import PdfFileReader
 
 
-app = Flask(__name__)
+
+app = Flask(__name__, static_folder='/')
 app.secret_key = "xxx007"
 
 AWS_ACCESS_KEY_ID = 'AKIA5BVJA3S5MNPVO2MP'
@@ -113,6 +116,48 @@ def ask():
             return render_template("indexSplit.html", question=question, theme=theme, response=response, key=key, files=files, model=model)
     else:
         return render_template('bad_key.html', question=question, theme=theme)
+    
+    
+def search_pdf_files(keyword, directory):
+    results = {}
+    encrypted_files = []  # List to store encrypted files
+    for root, _, files in os.walk(directory):
+        for file in files:
+            if file.endswith('.pdf'):
+                filepath = os.path.join(root[len(directory) - 1:], file)
+                try:
+                    with open(filepath, 'rb') as pdf_file:
+                        pdf_reader = PdfFileReader(pdf_file)
+                        if pdf_reader.isEncrypted:
+                            print(f"Skipping encrypted file: {filepath}")
+                            encrypted_files.append(filepath)  # Add the encrypted file to the list
+                            continue
+                        for page_num in range(pdf_reader.getNumPages()):
+                            text = pdf_reader.getPage(page_num).extractText()
+                            pattern = re.compile(r'(?<=\.)([^.]*\b{}\b[^.]*(?:\.[^.]*){{0,1}})'.format(keyword))
+                            matches = pattern.findall(text)
+                            if matches:
+                                if filepath not in results:
+                                    results[filepath] = []
+                                results[filepath].extend([(page_num, match) for match in matches])
+                except Exception as e:
+                    print(f"Error processing {filepath}: {str(e)}")
+    return results, encrypted_files
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    search_results = {}
+    encrypted_files = []
+    if request.method == 'POST':
+        keyword = request.form['keyword']
+        directory = "/"  # Replace with the specific directory you want to search
+        search_results, encrypted_files = search_pdf_files(keyword, directory)
+    return render_template('index.html', results=search_results, encrypted_files=encrypted_files)
+
+    
+    
+    
 t = Thread(target=initialize_ai)
 t.start()
 app.run(host='0.0.0.0', port=5000)
