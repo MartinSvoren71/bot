b707ab2f6b1bc6b0e0c5705033239d528135c22a
MartinSvoren71 <79747942+MartinSvoren71@users.noreply.github.com>
2023-03-29 19:58:00 +0200

    s

--- .ipynb_checkpoints/app2-checkpoint.py
+++ .ipynb_checkpoints/app2-checkpoint.py
@@ -155,12 +155,33 @@ def index2():
         search_results, encrypted_files = search_pdf_files(keyword, directory)
     return render_template('indexSplit .html', results=search_results, encrypted_files=encrypted_files)
 
-@app.route('/search_pdf_files', methods=['POST'])
-def search_files():
-    keyword = request.form['keyword']
-    directory = "/"  # Replace with the specific directory you want to search
-    search_results, encrypted_files = search_pdf_files(keyword, directory)
-    return jsonify(results=search_results, encrypted_files=encrypted_files)  
+def search_pdf_files(keyword, directory):
+    results = {}
+    encrypted_files = []  # List to store encrypted files
+    for root, _, files in os.walk(directory):
+        for file in files:
+            if file.endswith('.pdf'):
+                filepath = os.path.join(root[len(directory) - 1:], file)
+                print(f"Processing file: {filepath}")  # Add this line
+                try:
+                    with open(filepath, 'rb') as pdf_file:
+                        pdf_reader = PdfFileReader(pdf_file)
+                        if pdf_reader.isEncrypted:
+                            print(f"Skipping encrypted file: {filepath}")
+                            encrypted_files.append(filepath)  # Add the encrypted file to the list
+                            continue
+                        for page_num in range(pdf_reader.getNumPages()):
+                            text = pdf_reader.getPage(page_num).extractText()
+                            pattern = re.compile(r'(?<=\.)([^.]*\b{}\b[^.]*(?:\.[^.]*){{0,1}})'.format(keyword))
+                            matches = pattern.findall(text)
+                            if matches:
+                                if filepath not in results:
+                                    results[filepath] = []
+                                results[filepath].extend([(page_num, match) for match in matches])
+                except Exception as e:
+                    print(f"Error processing {filepath}: {str(e)}")
+    return results, encrypted_files
+
     
     
 t = Thread(target=initialize_ai)
