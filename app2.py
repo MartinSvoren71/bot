8866a9821ff5c56984a6eb62b20955ac76cb9603
MartinSvoren71 <79747942+MartinSvoren71@users.noreply.github.com>
2023-03-31 01:36:20 +0200

    s

--- app2.py
+++ app2.py
@@ -10,7 +10,7 @@ import json
 import subprocess
 import boto3
 from PyPDF4 import PdfFileReader
-import io
+import io import BytesIO
 folder_name = 's3/data/'
 app = Flask(__name__, static_folder='/')
 app.secret_key = "xxx007"
