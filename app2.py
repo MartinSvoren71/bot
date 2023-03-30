fcef49c6a7b2ba8f619fd66ffab5c5033980b1e3
MartinSvoren71 <79747942+MartinSvoren71@users.noreply.github.com>
2023-03-30 19:46:26 +0200

    a

--- app2.py
+++ app2.py
@@ -100,7 +100,7 @@ def generate_presigned_url(bucket, key, expiration=3600):
     return response
 
 @app.route('/ask_GPT', methods=['POST'])
-def ask_GPT  ask_GPT(keyword, file_paths):
+def ask_GPT(keyword, file_paths):
     question = request.form['question']
     theme = request.form['theme']
     model = request.form['model']
@@ -112,15 +112,15 @@ def ask_GPT  ask_GPT(keyword, file_paths):
     if key == "nnp":  # Check if the key is "xxx007"
         if theme == "general" :
             response = ask_GPT(question, model, theme)  # Pass the theme value
-            return render_template("indexSplit.html", question=question, response=response, key=key, files=files, model=model, theme=theme,results={})
+            #return render_template("indexSplit.html", question=question, response=response, key=key, files=files, model=model, theme=theme,results={})
         else :
             response = ask_ai(question, theme, model)  # Pass the theme value
-            return render_template("indexSplit.html", question=question, theme=theme, response=response, key=key, files=files, model=model, results={})
+           # return render_template("indexSplit.html", question=question, theme=theme, response=response, key=key, files=files, model=model, results={})
     else:
         return render_template('bad_key.html', question=question, theme=theme)
     
     
-def search_pdf_filesdef search_pdf_files(keyword, file_paths):
+def search_pdf_filesdef (keyword, file_paths):
     results = {}
     encrypted_files = []  # List to store encrypted files
 
