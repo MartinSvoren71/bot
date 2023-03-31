2ee5859b2040b99e2b665c8b67311d029fdfd6e3
MartinSvoren71 <79747942+MartinSvoren71@users.noreply.github.com>
2023-03-31 22:17:19 +0200

    sa

--- jsonize.py
+++ jsonize.py
@@ -1,71 +1,61 @@
-from llama_index import SimpleDirectoryReader, GPTListIndex, readers, GPTSimpleVectorIndex, LLMPredictor, PromptHelper
-from langchain import OpenAI
-import sys
 import os
 import boto3
-from IPython.display import Markdown, display
+from llama_index import SimpleDirectoryReader, GPTSimpleVectorIndex, LLMPredictor, PromptHelper
+from langchain import OpenAI
 from main import api_kx
-api_key=api_kx
 
-folder_name = 's3/data/coherent_chameleon'
 AWS_ACCESS_KEY_ID = 'your_access_key'
 AWS_SECRET_ACCESS_KEY = 'your_secret_key'
 AWS_DEFAULT_REGION = 'eu-north-1'
 BUCKET_NAME = 'knowledgevortex'
-s3_client = boto3.client(
-    's3',
-    aws_access_key_id=AWS_ACCESS_KEY_ID,
-    aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
-    region_name=AWS_DEFAULT_REGION
-)
 
-def initialize_ai(api_key):
-    os.environ["OPENAI_API_KEY"] = api_key
+def initialize_ai(api_key_value):
+    os.environ["OPENAI_API_KEY"] = api_key_value
 
-def construct_index(directory_path, api_key, bucket_name):
-    max_input_size = 4096
-    num_outputs = 2000
-    max_chunk_overlap = 20
-    chunk_size_limit = 600
-
-    openai.api_key = api_key
-    llm_predictor = LLMPredictor(llm=OpenAI(temperature=0.5, model_name="text-curie-001", max_tokens=num_outputs))
-    prompt_helper = PromptHelper(max_input_size, num_outputs, max_chunk_overlap, chunk_size_limit=chunk_size_limit)
-
+def download_files_from_s3(bucket_name, directory_path):
     s3 = boto3.resource('s3')
     bucket = s3.Bucket(bucket_name)
-    documents = SimpleDirectoryReader(directory_path).load_data()
-    for doc in documents:
-        s3_object = bucket.Object(f"{directory_path}/{doc}")
-        s3_object.put(Body=doc)
+    for obj in bucket.objects.filter(Prefix=directory_path):
+        if obj.key.endswith(".json"):
+            bucket.download_file(obj.key, obj.key.split("/")[-1])
 
-    index = GPTSimpleVectorIndex(
-        documents, llm_predictor=llm_predictor, prompt_helper=prompt_helper
-    )
+def upload_file_to_s3(file_path, bucket_name, object_key):
+    s3 = boto3.resource('s3')
+    s3.Object(bucket_name, object_key).put(Body=open(file_path, "rb"))
 
-    index.save_to_disk('index.json')
-
-    return index
-
-def check_and_construct_index(directory_path, api_key, bucket_name):
+ddef construct_index(directory_path, api_key, bucket_name):
     index_file = "index.json"
-
-    if not os.path.exists(index_file):
-        print(f"Constructing index from data in {directory_path}...")
-        index = construct_index(directory_path, api_key, bucket_name)
-        print("Index constructed and saved to disk.")
-    else:
-        index = GPTSimpleVectorIndex.load_from_disk('index.json')
-
-    return index
-
-def main():
-    bucket_name = 'knowledgevortex'
-
-    data_directory = "context_data/data"
-    index_file = "index.json"
-
-    index = check_and_construct_index(data_directory, api_key, bucket_name)
-
-    print("\I am done.\n")
     
+    if not os.path.exists(index_file):
+        max_input_size = 4096
+        num_outputs = 2000
+        max_chunk_overlap = 20
+        chunk_size_limit = 600
+
+        os.environ["OPENAI_API_KEY"] = api_key
+        openai.api_key = api_key
+        llm_predictor = LLMPredictor(llm=OpenAI(temperature=0.5, model_name="text-curie-001", max_tokens=num_outputs))
+        prompt_helper = PromptHelper(max_input_size, num_outputs, max_chunk_overlap, chunk_size_limit=chunk_size_limit)
+
+        s3 = boto3.resource('s3')
+        bucket = s3.Bucket(bucket_name)
+        for obj in bucket.objects.filter(Prefix=directory_path):
+            if obj.key.endswith(".json"):
+                bucket.download_file(obj.key, obj.key.split("/")[-1])
+
+        documents = SimpleDirectoryReader(directory_path).load_data()
+
+        index = GPTSimpleVectorIndex(
+            documents, llm_predictor=llm_predictor, prompt_helper=prompt_helper
+        )
+
+        index.save_to_disk(index_file)
+
+        s3.Object(bucket_name, f"{directory_path}/{index_file}").put(Body=open(index_file, "rb"))
+        
+        os.remove(index_file)
+
+        return index
+
+    else:
+        return GPTSimpleVectorIndex.load_from_disk(index_file)
