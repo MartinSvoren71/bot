6f7d5b43e6f91145fdfbd3e546391717e723b10f
MartinSvoren71 <79747942+MartinSvoren71@users.noreply.github.com>
2023-04-01 23:48:57 +0200

    da

--- .ipynb_checkpoints/app3-checkpoint.py
+++ .ipynb_checkpoints/app3-checkpoint.py
@@ -10,6 +10,14 @@ def get_subfolders_recursive(path):
             subfolders.append(os.path.relpath(os.path.join(root, d), path))
     return subfolders
 
+def get_files_recursive(path):
+    all_files = []
+    for root, _, files in os.walk(path):
+        for f in files:
+            rel_path = os.path.relpath(os.path.join(root, f), path)
+            all_files.append(rel_path)
+    return all_files
+
 @app.route('/')
 def index():
     data_folders = get_subfolders_recursive('Data/')
@@ -19,7 +27,7 @@ def index():
 def get_folder_content():
     selected_folder = request.form['selected_folder']
     folder_path = f'Data/{selected_folder}'
-    folder_content = [f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
+    folder_content = get_files_recursive(folder_path)
     return {'folder_content': folder_content}
 
 app.run(host='0.0.0.0', port=5000)
