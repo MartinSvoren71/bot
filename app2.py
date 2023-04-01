7a67349e188e85fe38aaba956f7fbb97eec08234
MartinSvoren71 <79747942+MartinSvoren71@users.noreply.github.com>
2023-04-01 17:43:56 +0200

    cs

--- app2.py
+++ app2.py
@@ -79,7 +79,19 @@ def index():
         return redirect(url_for("login"))
 
     
-    
+@app.route("/get_folders", methods=["GET"])
+def get_folders():
+    folder_path = "Data/Coherent/Chameleon/"   # those are used for listing pdf files 
+    files = []
+    folders = list_folders()
+
+    for root, dirnames, filenames in os.walk(folder_path):
+        for filename in filenames:
+            if not filename.startswith('.'):  # Ignore hidden files
+                file = {}
+                file["Key"] = os.path.join(root, filename)
+                file["PresignedURL"] = url_for("static", filename=file["Key"])
+                files.append(file)    
 
     
     
