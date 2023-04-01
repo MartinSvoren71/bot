ae36bb0d9a0a96e70f5557ea91bfe2b07cc529b0
MartinSvoren71 <79747942+MartinSvoren71@users.noreply.github.com>
2023-04-01 23:36:40 +0200

    da

--- .ipynb_checkpoints/app3-checkpoint.py
+++ .ipynb_checkpoints/app3-checkpoint.py
@@ -3,15 +3,19 @@ import os
 
 app = Flask(__name__)
 
+def get_subfolders(path):
+    return [d for d in os.listdir(path) if os.path.isdir(os.path.join(path, d))]
+
 @app.route('/')
 def index():
-    data_folders = os.listdir('Data/')
-    return render_template('results2.html', folders=data_folders)
+    data_folders = get_subfolders('Data/')
+    return render_template('index.html', folders=data_folders)
 
 @app.route('/get_folder_content', methods=['POST'])
 def get_folder_content():
     selected_folder = request.form['selected_folder']
-    folder_content = os.listdir(f'Data/{selected_folder}')
+    folder_path = f'Data/{selected_folder}'
+    folder_content = [f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
     return {'folder_content': folder_content}
 
 app.run(host='0.0.0.0', port=5000)
