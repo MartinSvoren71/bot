e13f00cf219290841213d68c54a88bdf241ea4de
MartinSvoren71 <79747942+MartinSvoren71@users.noreply.github.com>
2023-04-01 16:47:14 +0200

    das

--- .ipynb_checkpoints/app2-checkpoint.py
+++ .ipynb_checkpoints/app2-checkpoint.py
@@ -220,18 +220,13 @@ def list_folders_route():
 
 @app.route('/list_files')
 def list_files():
-        folder_path = "{folders}"   # those are used for listing pdf files   folders
+    folder_path = "{folders}"   # those are used for listing pdf files   folders
+        files = []
         folders = list_folders_route()
 
-        for root, dirnames, filenames in os.walk(folder_path):
-            for filename in filenames:
-                if not filename.startswith('.'):  # Ignore hidden files
-                    file = {}
-                    file["Key"] = os.path.join(root, filename)
-                    file["PresignedURL"] = url_for("static", filename=file["Key"])
-                    files.append(file)
+       
     files = []
-    for root, dirnames, filenames in os.walk(folder_path):
+    for root, dirnames, filenames in os.walk(folders):
         for filename in filenames:
             if not filename.startswith('.'):  # Ignore hidden files
                 file = {}
