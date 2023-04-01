19d02e6c01a17c80d4a15e5bea84424965aa3b59
MartinSvoren71 <79747942+MartinSvoren71@users.noreply.github.com>
2023-04-01 11:57:22 +0200

    das

--- .ipynb_checkpoints/app2-checkpoint.py
+++ .ipynb_checkpoints/app2-checkpoint.py
@@ -58,7 +58,7 @@ def index():
             {options}
         </select>
         '''
-       
+        folder_path = "/data/" + folder_name
         files = []
         for file_name in os.listdir(folder_path):
             if os.path.isfile(os.path.join(folder_path, file_name)):
