1b34e9b1930f6677fe2a18a5626cbd71951b0fa8
MartinSvoren71 <79747942+MartinSvoren71@users.noreply.github.com>
2023-04-01 16:46:01 +0200

    das

--- .ipynb_checkpoints/app2-checkpoint.py
+++ .ipynb_checkpoints/app2-checkpoint.py
@@ -221,7 +221,6 @@ def list_folders_route():
 @app.route('/list_files')
 def list_files():
         folder_path = "{folders}"   # those are used for listing pdf files   folders
-        files = []
         folders = list_folders_route()
 
         for root, dirnames, filenames in os.walk(folder_path):
