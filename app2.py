7c8625957ec1b9952815476ea043d0de19fd63b2
MartinSvoren71 <79747942+MartinSvoren71@users.noreply.github.com>
2023-04-02 12:02:00 +0200

    da

--- app2.py
+++ app2.py
@@ -196,7 +196,7 @@ def search_files():
     encrypted_files = []
 
     # Set the folder path to search for PDF files
-   # selected_folder = request.form['selected_folder']
+    selected_folder = request.form['selected_folder']
     folder_path = f'Data/{selected_folder}'
 
     if request.method == 'POST':
