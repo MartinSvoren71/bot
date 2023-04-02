d76f9fe1d1151bdb9b6d2ef819cd2f884648b511
MartinSvoren71 <79747942+MartinSvoren71@users.noreply.github.com>
2023-04-02 18:19:35 +0200

    s

--- ask_GPT.py
+++ ask_GPT.py
@@ -18,7 +18,7 @@ initialize_GPT(api_k)
 def ask_GPT(question):
     os.environ["OPENAI_API_KEY"] = api_kx
     response = openai.Completion.create(
-        engine="gpt-3.5-turbo",
+        engine="text-davinci-003",
         prompt=question,
         max_tokens=1550,
         n=1,
