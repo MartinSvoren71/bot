9b912b5d02b8488fefe496c5ac062cf825df177b
MartinSvoren71 <79747942+MartinSvoren71@users.noreply.github.com>
2023-04-03 21:02:42 +0200

    das

--- ask_ai.py
+++ ask_ai.py
@@ -33,23 +33,13 @@ def ask_ai(question, current_folder):
     
     # Write the new data followed by the existing data
     with open(log_file, "w") as f:
-        f.write("\n")
         f.write(f"Time: {datetime.datetime.now()}\n")
-        f.write("\n")
         f.write(f"Folder: {folder_path}\n")
-        f.write("\n")        
         f.write(f"Question: {question}\n")
-        f.write("\n")
-        f.write(f"Operator: {answer}\n")  # Replace response.answer with answer
-        f.write("\n")
-        f.write("====================================\n")
-        f.write("  LIB    Knowlege Vortex v1.5    \n")
-        f.write("============================\n")
-        f.write("=========================\n")
-        f.write("=====================\n")
-        f.write("==================\n")
-        f.write("==============\n")
-        f.write("===========\n")
+        f.write(f"Answer: {response.response}\n")               
+        f.write("======================================================================================\n")
+        f.write("                         Knowlege Vortex v1.1                                 \n")
+        f.write("======================================================================================\n")
         f.write(existing_data)
         
     # return response.response
