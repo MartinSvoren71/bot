3ff0b8a75ef9c75efd93b7cbdc245aba8d0609f6
MartinSvoren71 <79747942+MartinSvoren71@users.noreply.github.com>
2023-04-04 21:05:58 +0200

    asc

--- .ipynb_checkpoints/app2-checkpoint.py
+++ .ipynb_checkpoints/app2-checkpoint.py
@@ -253,21 +253,6 @@ def get_folder_content():
     current_folder = selected_folder
     return {'folder_content': folder_content}
 
-@app.route('/upload', methods=['POST'])
-def upload_file():
-    file = request.files['file']
-    filename = file.filename
-    file.save(filename)
-    flash('File uploaded successfully!')
-    return redirect(url_for('index'))
-
-@app.route('/delete/<filename>')
-def delete_file(filename):
-    os.remove(filename)
-    flash('File deleted successfully!')
-    return redirect(url_for('index'))
-
-
 
 
 
@@ -275,109 +260,88 @@ def delete_file(filename):
 @app.route('/filemanager/')
 @app.route('/filemanager/<path:subpath>')
 def file_manager(subpath=None):
-    if "logged_in" in session:
-        if subpath:
-            dir_path = os.path.join(app.config['UPLOAD_FOLDER'], subpath)
-        else:
-            dir_path = app.config['UPLOAD_FOLDER']
-
-        if not os.path.exists(dir_path):
-            flash('Directory not found.')
-            return redirect(url_for('file_manager'))
-
-        items = os.listdir(dir_path)
-        files = []
-        folders = []
-
-        for item in items:
-            item_path = os.path.join(dir_path, item)
-            if os.path.isfile(item_path):
-                files.append(item)
-            elif os.path.isdir(item_path):
-                folders.append(item)
-
-        return render_template('file_manager.html', files=files, folders=folders, subpath=subpath)
+    if subpath:
+        dir_path = os.path.join(app.config['UPLOAD_FOLDER'], subpath)
     else:
-        flash("Please log in first")
-        return redirect(url_for("login"))
+        dir_path = app.config['UPLOAD_FOLDER']
+
+    if not os.path.exists(dir_path):
+        flash('Directory not found.')
+        return redirect(url_for('file_manager'))
+
+    items = os.listdir(dir_path)
+    files = []
+    folders = []
+
+    for item in items:
+        item_path = os.path.join(dir_path, item)
+        if os.path.isfile(item_path):
+            files.append(item)
+        elif os.path.isdir(item_path):
+            folders.append(item)
+
+    return render_template('file_manager.html', files=files, folders=folders, subpath=subpath)
 
 @app.route('/upload/', methods=['POST'])
 @app.route('/upload/<path:subpath>', methods=['POST'])
 def upload(subpath=None):
-    if "logged_in" in session:
-        if request.method == 'POST':
-            files = request.files.getlist('files')
-            if files:
-                if subpath:
-                    save_path = os.path.join(app.config['UPLOAD_FOLDER'], subpath)
-                else:
-                    save_path = app.config['UPLOAD_FOLDER']
+    if request.method == 'POST':
+        files = request.files.getlist('files')
+        if files:
+            if subpath:
+                save_path = os.path.join(app.config['UPLOAD_FOLDER'], subpath)
+            else:
+                save_path = app.config['UPLOAD_FOLDER']
 
-                for file in files:
-                    if isinstance(file, FileStorage):
-                        file_path = os.path.join(save_path, secure_filename(file.filename))
-                        os.makedirs(os.path.dirname(file_path), exist_ok=True)
-                        file.save(file_path)
+            for file in files:
+                if isinstance(file, FileStorage):
+                    file_path = os.path.join(save_path, secure_filename(file.filename))
+                    os.makedirs(os.path.dirname(file_path), exist_ok=True)
+                    file.save(file_path)
 
-                flash('Files uploaded successfully.')
-                return redirect(url_for('file_manager', subpath=subpath))
-        flash('Error uploading files.')
-        return redirect(url_for('file_manager', subpath=subpath))
-    else:
-        flash("Please log in first")
-        return redirect(url_for("login"))
+            flash('Files uploaded successfully.')
+            return redirect(url_for('file_manager', subpath=subpath))
+    flash('Error uploading files.')
+    return redirect(url_for('file_manager', subpath=subpath))
 
 @app.route('/delete/<path:filename>')
 def delete(filename):
-    if "logged_in" in session:
-        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-        if os.path.exists(file_path):
-            os.remove(file_path)
-            flash('File deleted successfully.')
-        else:
-            flash('File not found.')
-        return redirect(url_for('file_manager'))
+    file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    if os.path.exists(file_path):
+        os.remove(file_path)
+        flash('File deleted successfully.')
     else:
-        flash("Please log in first")
-        return redirect(url_for("login"))
+        flash('File not found.')
+    return redirect(url_for('file_manager'))
 
 @app.route('/create_folder/', methods=['POST'])
 @app.route('/create_folder/<path:subpath>', methods=['POST'])
 def create_folder(subpath=None):
-    if "logged_in" in session:
-        folder_name = request.form['folder_name']
-        if subpath:
-            parent_path = os.path.join(app.config['UPLOAD_FOLDER'], subpath)
-        else:
-            parent_path = app.config['UPLOAD_FOLDER']
-        
-        new_folder_path = os.path.join(parent_path, folder_name)
-        if not os.path.exists(new_folder_path):
-            os.makedirs(new_folder_path)
-            flash('Folder created successfully.')
-        else:
-            flash('Folder already exists.')
-
-        return redirect(url_for('file_manager', subpath=subpath))
+    folder_name = request.form['folder_name']
+    if subpath:
+        parent_path = os.path.join(app.config['UPLOAD_FOLDER'], subpath)
     else:
-        flash("Please log in first")
-        return redirect(url_for("login"))
+        parent_path = app.config['UPLOAD_FOLDER']
+    
+    new_folder_path = os.path.join(parent_path, folder_name)
+    if not os.path.exists(new_folder_path):
+        os.makedirs(new_folder_path)
+        flash('Folder created successfully.')
+    else:
+        flash('Folder already exists.')
+
+    return redirect(url_for('file_manager', subpath=subpath))
 
 @app.route('/delete_folder/<path:folder_path>')
 def delete_folder(folder_path):
-    if "logged_in" in session:
-        full_folder_path = os.path.join(app.config['UPLOAD_FOLDER'], folder_path)
-        if os.path.exists(full_folder_path):
-            shutil.rmtree(full_folder_path)
-            flash('Folder deleted successfully.')
-        else:
-            flash('Folder not found.')
-
-        return redirect(url_for('file_manager'))
+    folder_path = os.path.join(app.config['UPLOAD_FOLDER'], folder_path)
+    if os.path.exists(folder_path) and os.path.isdir(folder_path):
+        shutil.rmtree(folder_path)
+        flash('Folder deleted successfully.')
     else:
-        flash("Please log in first")
-        return redirect(url_for("login"))
+        flash('Folder not found or not a directory.')
 
+    return redirect(url_for('file_manager'))
 
 #runn app as local on port 5000 , accesible on private and public AWS IP
 app.run(host='0.0.0.0', port=5000)
