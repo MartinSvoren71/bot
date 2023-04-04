a559b83f86604b40e526ee3b2bb5b8a4c2b3c8ac
MartinSvoren71 <79747942+MartinSvoren71@users.noreply.github.com>
2023-04-04 19:28:50 +0200

    cas

--- folder.py
+++ folder.py
@@ -1,22 +1,22 @@
-from flask import Flask, render_template, request, send_from_directory, flash, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 import os
-
+import shutil
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = 'Data'
-app.secret_key = 'your_secret_key'
-
+app.config['SECRET_KEY'] = 'mysecretkey'
 @app.route('/')
 def index():
-    folders = os.listdir(app.config['UPLOAD_FOLDER'])
-    return render_template('index.html', folders=folders)
-
-@app.route('/files', methods=['POST'])
-def list_files():
-    selected_folder = request.form.get('selected_folder')
-    if selected_folder:
-        file_list = os.listdir(os.path.join(app.config['UPLOAD_FOLDER'], selected_folder))
-        return {'files': file_list, 'selected_folder': selected_folder}
-    return {'error': 'No folder selected'}
-
+    files = os.listdir()
+    return render_template('index.html', files=files)
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    file = request.files['file']
+    filename = file.filename
+    file.save(filename)
+    flash('File uploaded successfully!')
+    return redirect(url_for('index'))
+@app.route('/delete/<filename>')
+def delete_file(filename):
+    os.remove(filename)
+    flash('File deleted successfully!')
+    return redirect(url_for('index'))
 app.run(host='0.0.0.0', port=5000)
-
