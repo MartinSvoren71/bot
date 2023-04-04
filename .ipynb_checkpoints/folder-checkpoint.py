69a1fa37681c29aa11066714b1468cc3efadc9df
MartinSvoren71 <79747942+MartinSvoren71@users.noreply.github.com>
2023-04-04 19:00:02 +0200

    dsa

--- .ipynb_checkpoints/folder-checkpoint.py
+++ .ipynb_checkpoints/folder-checkpoint.py
@@ -5,15 +5,21 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mysecretkey'
 @app.route('/')
 def index():
-    files = os.listdir()
-    return render_template('folder.html', files=files)
-@app.route('/Data', methods=['POST'])
+    folders = [f for f in os.listdir("Data/") if os.path.isdir(os.path.join("Data/", f))]
+    return render_template('index.html', folders=folders)
+
+@app.route('/upload', methods=['POST'])
 def upload_file():
+    folder = request.form['folder']
+    new_folder = request.form['new_folder']
+    if new_folder:
+        os.mkdir(os.path.join("Data/", folder, new_folder))
     file = request.files['file']
     filename = file.filename
-    file.save(filename)
+    file.save(os.path.join("Data/", folder, filename))
     flash('File uploaded successfully!')
     return redirect(url_for('index'))
+
 @app.route('/delete/<filename>')
 def delete_file(filename):
     os.remove(filename)
