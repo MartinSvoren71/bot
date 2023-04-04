f8cd2d5a210fb84f8d31c6e1c6f8e14761b4badb
MartinSvoren71 <79747942+MartinSvoren71@users.noreply.github.com>
2023-04-04 19:49:35 +0200

    dsa

--- file_manager/app.py
+++ file_manager/app.py
@@ -9,10 +9,35 @@ app.secret_key = 'your_secret_key'
 def index():
     return render_template('index.html')
 
+# ...
+
 @app.route('/file_manager/')
-def file_manager():
-    files = os.listdir(app.config['UPLOAD_FOLDER'])
-    return render_template('file_manager.html', files=files)
+@app.route('/file_manager/<path:subpath>')
+def file_manager(subpath=None):
+    if subpath:
+        dir_path = os.path.join(app.config['UPLOAD_FOLDER'], subpath)
+    else:
+        dir_path = app.config['UPLOAD_FOLDER']
+
+    if not os.path.exists(dir_path):
+        flash('Directory not found.')
+        return redirect(url_for('file_manager'))
+
+    items = os.listdir(dir_path)
+    files = []
+    folders = []
+
+    for item in items:
+        item_path = os.path.join(dir_path, item)
+        if os.path.isfile(item_path):
+            files.append(item)
+        elif os.path.isdir(item_path):
+            folders.append(item)
+
+    return render_template('file_manager.html', files=files, folders=folders, subpath=subpath)
+
+# ...
+
 
 @app.route('/upload/', methods=['POST'])
 def upload():
