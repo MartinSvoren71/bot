bd89552dac473a6c709ee8b01f8b13cd53da01b3
MartinSvoren71 <79747942+MartinSvoren71@users.noreply.github.com>
2023-04-04 19:03:10 +0200

    sa

--- .ipynb_checkpoints/folder-checkpoint.py
+++ .ipynb_checkpoints/folder-checkpoint.py
@@ -28,5 +28,4 @@ def delete_file(filename):
     flash('File deleted successfully!')
     return redirect(url_for('index'))
 
-if __name__ == '__main__':
-    app.run(debug=True)
+app.run(host='0.0.0.0', port=5000)
