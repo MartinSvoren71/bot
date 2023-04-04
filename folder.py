93824f39774350fa98767419c92e76496b6bdf23
MartinSvoren71 <79747942+MartinSvoren71@users.noreply.github.com>
2023-04-04 19:02:47 +0200

    das

--- folder.py
+++ folder.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 import os
 import shutil
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mysecretkey'
+
 @app.route('/')
 def index():
     folders = [f for f in os.listdir("Data/") if os.path.isdir(os.path.join("Data/", f))]
@@ -22,10 +24,9 @@ def upload_file():
 
 @app.route('/delete/<filename>')
 def delete_file(filename):
-    os.remove(filename)
+    os.remove(os.path.join("Data/", filename))
     flash('File deleted successfully!')
     return redirect(url_for('index'))
 
-
-#runn app as local on port 5000 , accesible on private and public AWS IP
-app.run(host='0.0.0.0', port=5000)
+if __name__ == '__main__':
+    app.run(debug=True)
