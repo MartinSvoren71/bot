2fa84f68d60a3631282a33091583d2469477a08b
MartinSvoren71 <79747942+MartinSvoren71@users.noreply.github.com>
2023-04-04 19:38:54 +0200

    csa

--- file_manager/.ipynb_checkpoints/app-checkpoint.py
+++ file_manager/.ipynb_checkpoints/app-checkpoint.py
@@ -35,5 +35,4 @@ def delete(filename):
         flash('File not found.')
     return redirect(url_for('file_manager'))
 
-if __name__ == '__main__':
-    app.run(debug=True)
+app.run(host='0.0.0.0', port=5000)
