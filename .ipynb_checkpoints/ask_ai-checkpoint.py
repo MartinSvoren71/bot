bb55a1c8c9c4c10c3a16a94e308efd358e9764d1
MartinSvoren71 <79747942+MartinSvoren71@users.noreply.github.com>
2023-04-07 17:01:21 +0200

    da

--- .ipynb_checkpoints/ask_ai-checkpoint.py
+++ .ipynb_checkpoints/ask_ai-checkpoint.py
@@ -72,9 +72,9 @@ def construct_index(current_folder):
 
 # define prompt helper
 # set maximum input size
-    max_input_size = 5096
+    max_input_size = 4096
 # set number of output tokens
-    num_output = 500
+    num_output = 256
 # set maximum chunk overlap
     max_chunk_overlap = 20
     prompt_helper = PromptHelper(max_input_size, num_output, max_chunk_overlap)
