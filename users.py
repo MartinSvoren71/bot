8b32c9b1ec3aa3845b4dcf1c1ec6c64a02c664c1
MartinSvoren71 <79747942+MartinSvoren71@users.noreply.github.com>
2023-04-24 11:51:31 +0200

    das

---
+++ users.py
@@ -0,0 +1,56 @@
+from flask import Flask, render_template, request, jsonify
+import json
+
+app = Flask(__name__)
+
+def load_users():
+    with open('user.json', 'r') as file:
+        users = json.load(file)
+    return users
+
+def save_users(users):
+    with open('user.json', 'w') as file:
+        json.dump(users, file)
+
+@app.route('/')
+def index():
+    return render_template('create_user.html')
+
+@app.route('/create_user', methods=['POST'])
+def create_user():
+    username = request.form['username']
+    password = request.form['password']
+
+    user = {
+        "username": username,
+        "password": password
+    }
+
+    users = load_users()
+    users.append(user)
+    save_users(users)
+
+    return jsonify({"status": "success", "message": "User created successfully."})
+
+@app.route('/get_users', methods=['GET'])
+def get_users():
+    users = load_users()
+    return jsonify(users)
+
+@app.route('/delete_user', methods=['POST'])
+def delete_user():
+    username = request.json['username']
+    users = load_users()
+    user_to_delete = None
+
+    for user in users:
+        if user['username'] == username:
+            user_to_delete = user
+            break
+
+    if user_to_delete:
+        users.remove(user_to_delete)
+        save_users(users)
+        return jsonify({"status": "success", "message": "User deleted successfully."})
+    else:
+        return
