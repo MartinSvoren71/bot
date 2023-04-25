0bd084a20c8c5d1bb10dcc18e22e93937feff8f0
MartinSvoren71 <79747942+MartinSvoren71@users.noreply.github.com>
2023-04-25 22:54:42 +0200

    das

--- .ipynb_checkpoints/ask_ai-checkpoint.py
+++ .ipynb_checkpoints/ask_ai-checkpoint.py
@@ -31,7 +31,7 @@ def ask_ai(question, current_folder):
     #documents = SimpleDirectoryReader(folder_path).load_data('index.json')
     #index = GPTSimpleVectorIndex.from_documents(documents)
     index = GPTSimpleVectorIndex.load_from_disk(index_file)
-    response = index.query(question, mode="default")  #
+    response = index.query(question, mode="tree_summarize")  #
     log_file = os.path.join(os.getcwd(), 'log.txt')
     # Read the existing data in the log file
     with open(log_file, "r") as f:
