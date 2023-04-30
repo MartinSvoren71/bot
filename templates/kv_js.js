14b56b8933bfc101443306a52369e40922fc0376
MartinSvoren71 <79747942+MartinSvoren71@users.noreply.github.com>
2023-04-30 20:28:56 +0200

    dsa

--- templates/kv_js.js
+++ templates/kv_js.js
@@ -326,3 +326,14 @@ var myLink = document.getElementById("results");
     event.preventDefault();
     // your custom code here
   });
+
+DecoupledEditor
+            .create( document.querySelector( '#editor' ) )
+            .then( editor => {
+                const toolbarContainer = document.querySelector( '#toolbar-container' );
+
+                toolbarContainer.appendChild( editor.ui.view.toolbar.element );
+            } )
+            .catch( error => {
+                console.error( error );
+            } );
