bcb3149f24fcaef00bbd7185d51b3af1d83d99b1
MartinSvoren71 <79747942+MartinSvoren71@users.noreply.github.com>
2023-04-30 20:30:41 +0200

    das

--- templates/.ipynb_checkpoints/kv_js-checkpoint.js
+++ templates/.ipynb_checkpoints/kv_js-checkpoint.js
@@ -327,13 +327,3 @@ var myLink = document.getElementById("results");
     // your custom code here
   });
 
-DecoupledEditor
-            .create( document.querySelector( '#editor' ) )
-            .then( editor => {
-                const toolbarContainer = document.querySelector( '#toolbar-container' );
-
-                toolbarContainer.appendChild( editor.ui.view.toolbar.element );
-            } )
-            .catch( error => {
-                console.error( error );
-            } );
