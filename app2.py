b4e15c78f55f8f569f4683f9281ab7a2dbf5df38
MartinSvoren71 <79747942+MartinSvoren71@users.noreply.github.com>
2023-05-01 20:06:00 +0200

    dsa

--- app2.py
+++ app2.py
@@ -114,8 +114,8 @@ def index():
         if theme == "light" :
             return render_template("indexSplit_light.html", folders=data_folders+customer_data_folders, files=files, results={})
         else :
-            #return render_template("indexSplit.html", folders=data_folders+customer_data_folders, files=files, results={})
             return render_template("indexSplit.html", folders=data_folders+customer_data_folders, files=files, results={})
+            #return render_template("indexSplit.html", folders=data_folders+customer_data_folders, files=files, results={})
     else:
         flash("Please log in first")
         return redirect(url_for("login"))
