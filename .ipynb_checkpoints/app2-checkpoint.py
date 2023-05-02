70d3b763c81fac728d9ef2ce3172bf75be292651
MartinSvoren71 <79747942+MartinSvoren71@users.noreply.github.com>
2023-05-02 14:41:20 +0200

    das

--- .ipynb_checkpoints/app2-checkpoint.py
+++ .ipynb_checkpoints/app2-checkpoint.py
@@ -95,7 +95,7 @@ def list_files_and_urls(folder_path):
             if not filename.startswith('.'):  # Ignore hidden files
                 file = {}
                 file["Key"] = os.path.join(root, filename)
-                file["PresignedURL"] = url_for("static", filename=file["Key"])
+                #file["PresignedURL"] = url_for("static", filename=file["Key"])
                 files.append(file)
     return files
 
