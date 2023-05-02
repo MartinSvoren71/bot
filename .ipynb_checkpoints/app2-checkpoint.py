a4ac94c1eec7a6804a62e4018d5f4ca046de6dd9
MartinSvoren71 <79747942+MartinSvoren71@users.noreply.github.com>
2023-05-02 16:11:38 +0200

    dsa

--- .ipynb_checkpoints/app2-checkpoint.py
+++ .ipynb_checkpoints/app2-checkpoint.py
@@ -71,7 +71,7 @@ def bad_key():
 @app.route("/indexSplit", methods=["GET", "POST"])
 def index():
     if "logged_in" in session:
-        
+        username = session["username"]
         theme_sel = "dark"
         theme=theme_sel
         data_folders = get_subfolders_recursive('Data/')
@@ -222,7 +222,7 @@ def list_folders():
 # function for splitting path and generating subfolder path
 
 def get_subfolders_recursive(path):
-    username = session["username"]
+    
     subfolders = []
     for root, dirs, _ in os.walk(path):
         for d in dirs:
