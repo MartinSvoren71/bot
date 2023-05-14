721b3075cc5c4972ad69422c9ebb1f076701c713
MartinSvoren71 <79747942+MartinSvoren71@users.noreply.github.com>
2023-05-14 20:13:46 +0200

    DAS

--- templates/kv_js.js
+++ templates/kv_js.js
@@ -328,8 +328,8 @@ var myLink = document.getElementById("results");
 
      tinymce.init({
       selector: '#editorX',
-      plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed linkchecker a11ychecker tinymcespellchecker permanentpen powerpaste advtable advcode editimage tinycomments tableofcontents footnotes mergetags autocorrect typography inlinecss',
-      toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
+      plugins: 'anchor autolink charmap codesample  image link lists media searchreplace table visualblocks wordcount checklist casechange   advtable advcode editimage tinycomments    typography inlinecss',
+      toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table  | addcomment showcomments |  typography | align lineheight | checklist numlist bullist indent outdent |  charmap | removeformat',
       tinycomments_mode: 'embedded',
       tinycomments_author: 'Author name',
       mergetags_list: [
