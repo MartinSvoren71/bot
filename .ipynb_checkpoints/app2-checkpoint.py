99c1a7e30b108b6a4c4472d20c4979096dbb1e68
MartinSvoren71 <79747942+MartinSvoren71@users.noreply.github.com>
2023-05-15 20:58:58 +0200

    das

--- .ipynb_checkpoints/app2-checkpoint.py
+++ .ipynb_checkpoints/app2-checkpoint.py
@@ -170,7 +170,7 @@ def process_pdf_file(filepath, keyword, threshold):
             words = page_text.split()
             for i in range(len(words)):
                 if fuzz.ratio(words[i].lower(), keyword.lower()) > threshold:
-                    surrounding_text = ' '.join(words[max(0, i - 5):min(len(words), i + 6)])  # 5 words before and after
+                    surrounding_text = ' '.join(words[max(0, i - 13):min(len(words), i + 14)])  # 5 words before and after
                     matches.append((page_num, surrounding_text))
     except Exception as e:
         print(f"Error processing file {filepath}: {e}")
