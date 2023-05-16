af2742bc2f2dc506c087981ca364d8c01200fea5
MartinSvoren71 <79747942+MartinSvoren71@users.noreply.github.com>
2023-05-16 20:04:09 +0200

    csa

--- .ipynb_checkpoints/check_service-checkpoint.sh
+++ .ipynb_checkpoints/check_service-checkpoint.sh
@@ -8,33 +8,38 @@ get_public_ip() {
 # Define the port to check
 PORT=5000
 
-# Retrieve the current public IP address
-CURRENT_IP=$(get_public_ip)
+while true; do
+    # Retrieve the current public IP address
+    CURRENT_IP=$(get_public_ip)
 
-# Function to check if the IP and port are responsive
-check_availability() {
-    nc -zv -w 5 $CURRENT_IP $PORT > /dev/null 2>&1
-}
+    # Function to check if the IP and port are responsive
+    check_availability() {
+        nc -zv -w 5 $CURRENT_IP $PORT > /dev/null 2>&1
+    }
 
-# Check if the IP and port are responsive
-if check_availability; then
-    echo "IP $CURRENT_IP is responsive"
-else
-    echo "IP $CURRENT_IP is not responsive. Restarting app2.service..."
-    
-    # Restart the app2.service
-    systemctl restart app2.service
-    
-    # Wait for the service to start
-    sleep 5
-    
-    # Retrieve the new public IP address after restart
-    NEW_IP=$(get_public_ip)
-    
-    # Check if the new IP and port are responsive
+    # Check if the IP and port are responsive
     if check_availability; then
-        echo "Restart successful. New IP $NEW_IP is now responsive."
+        echo "IP $CURRENT_IP is responsive"
     else
-        echo "Restart failed. New IP $NEW_IP is still not responsive."
+        echo "IP $CURRENT_IP is not responsive. Restarting app2.service..."
+        
+        # Restart the app2.service
+        systemctl restart app2.service
+        
+        # Wait for the service to start
+        sleep 5
+        
+        # Retrieve the new public IP address after restart
+        NEW_IP=$(get_public_ip)
+        
+        # Check if the new IP and port are responsive
+        if check_availability; then
+            echo "Restart successful. New IP $NEW_IP is now responsive."
+        else
+            echo "Restart failed. New IP $NEW_IP is still not responsive."
+        fi
     fi
-fi
+    
+    # Wait for 10 seconds before running the loop again
+    sleep 10
+done
