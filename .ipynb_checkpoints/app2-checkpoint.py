959ff3e6ddda0c5a9342440d6fee17144e064418
MartinSvoren71 <79747942+MartinSvoren71@users.noreply.github.com>
2023-05-27 15:59:57 +0200

    ds

--- .ipynb_checkpoints/app2-checkpoint.py
+++ .ipynb_checkpoints/app2-checkpoint.py
@@ -29,6 +29,8 @@ from flask_ckeditor import CKEditor
 from flask_session import Session
 import time 
 import threading
+from concurrent.futures import ThreadPoolExecutor
+from threading import Condition
 
 app = Flask(__name__, static_folder='/')
 ckeditor = CKEditor(app)
@@ -180,8 +182,9 @@ def ask_LIB_route():
 
 from fuzzywuzzy import fuzz
 
+# Initialise the threading condition
+pause_condition = Condition()
 
-# part_1 process search on pdf files
 def process_pdf_file(filepath, keyword, pattern):
     matches = []
     is_encrypted = False
@@ -200,32 +203,30 @@ def process_pdf_file(filepath, keyword, pattern):
         return filepath, matches, is_encrypted
     return filepath, matches, is_encrypted
 
-pause_flag = True
-
-def set_pause_flag():
-    global pause_flag
+def set_pause_condition():
+    global pause_condition
     while True:
-        time.sleep(2)  # After every 5 seconds, set the pause_flag
-        pause_flag = True
-        time.sleep(2)  # Keep the flag set for 1 second
-        pause_flag = False
+        with pause_condition:
+            pause_condition.wait(4)  # After every 4 seconds, pause this condition
+            pause_condition.notify_all()  # Resume all threads after 1 second
 
-# part_2 process search on pdf files     
 def search_pdf_files(keyword, folder_path):
-    global pause_flag
+    global pause_condition
     results = {}
     encrypted_files = []
-    pattern = re.compile(r'(?<=\.)([^.]*\b{}\b[^.]*(?:\.[^.]*){{0,1}})'.format(keyword), re.I)  # added re.I flag
+    pattern = re.compile(r'(?<=\.)([^.]*\b{}\b[^.]*(?:\.[^.]*){{0,1}})'.format(keyword), re.I)
     pdf_files = [os.path.join(root, filename)
                  for root, _, filenames in os.walk(folder_path)
                  for filename in filenames
                  if filename.lower().endswith('.pdf')]
-    max_threads = 4  # Adjust this number as needed
+    
+    max_threads = os.cpu_count()  # Set to number of cores in CPU
+    
     with ThreadPoolExecutor(max_workers=max_threads) as executor:
         future_results = [executor.submit(process_pdf_file, filepath, keyword, pattern) for filepath in pdf_files]
         for future in future_results:
-            while pause_flag:  # If pause_flag is set, pause this thread
-                time.sleep(0.3)  # Sleep for a short duration to avoid busy waiting
+            with pause_condition:
+                pause_condition.wait()  # Pause this thread if condition is paused
             filepath, matches, is_encrypted = future.result()
             if is_encrypted:
                 encrypted_files.append(filepath)
@@ -235,10 +236,8 @@ def search_pdf_files(keyword, folder_path):
     return results, encrypted_files
 
 # Create and start the pause flag thread
-t_pause = threading.Thread(target=set_pause_flag)
+t_pause = threading.Thread(target=set_pause_condition)
 t_pause.start()
-
-# Now you can start your search threads
 
 # part_3 process search on pdf files     + caller from web app
 @app.route('/search_pdf_files', methods=['POST'])
